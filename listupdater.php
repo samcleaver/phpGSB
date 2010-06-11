65f0b7b76e28dff63963c1cb8ed8da0815c3c3b0
beaver6813 <cleaver.sam@gmail.com>
2010-06-11 10:32:26 +0000

    Reloaded Files

---
+++ listupdater.php
@@ -0,0 +1,18 @@
+<?php
+/*
+phpGSB - PHP Google Safe Browsing Implementation
+Version 0.1 (ALPHA) - Not recommended for production use
+Released under New BSD License (see LICENSE)
+Copyright (c) 2010, Sam Cleaver (Beaver6813, Beaver6813.com)
+All rights reserved.
+
+UPDATER EXAMPLE
+*/
+require("phpgsb.class.php");
+$phpgsb = new phpGSB("DATABASE_USERNAME","DATABASE_USERNAME","DATABASE_PASSWORD");
+//Obtain an API key from: http://code.google.com/apis/safebrowsing/key_signup.html
+$phpgsb->apikey = "API_KEY_HERE";
+$phpgsb->usinglists = array('googpub-phish-shavar','goog-malware-shavar');
+$phpgsb->runUpdate();
+$phpgsb->close();
+?>
---
+++ lookup.php
@@ -0,0 +1,21 @@
+<?php
+/*
+phpGSB - PHP Google Safe Browsing Implementation
+Version 0.1 (ALPHA) - Not recommended for production use
+Released under New BSD License (see LICENSE)
+Copyright (c) 2010, Sam Cleaver (Beaver6813, Beaver6813.com)
+All rights reserved.
+
+LOOKUP EXAMPLE
+*/
+require("phpgsb.class.php");
+$phpgsb = new phpGSB("DATABASE_USERNAME","DATABASE_USERNAME","DATABASE_PASSWORD");
+//Obtain an API key from: http://code.google.com/apis/safebrowsing/key_signup.html
+$phpgsb->apikey = "API_KEY_HERE";
+$phpgsb->usinglists = array('googpub-phish-shavar','goog-malware-shavar');
+//Should return false (not phishing or malware)
+var_dump($phpgsb->doLookup('http://www.google.com'));
+//Should return true, malicious URL
+var_dump($phpgsb->doLookup('http://www.gumblar.cn'));
+$phpgsb->close();
+?>
